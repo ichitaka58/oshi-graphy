5639867dbc12a3844d7aab53dcff4dea681c12a1
ichitaka58 <ichinosetakayuki92@gmail.com>
2026-07-30 11:05:34 +0900

    feat: ArtistControllerに対するCRUD操作を追加し、管理者専用のAPIルートを設定

--- app/Http/Controllers/Api/ArtistController.php
+++ app/Http/Controllers/Api/ArtistController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Artist;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class ArtistController extends Controller
 {
@@ -15,7 +16,62 @@ class ArtistController extends Controller
             ->orderBy('name')
             ->limit(20)
             ->get(['id', 'name']);
-        
+
         return response()->json($artists);
     }
+
+    public function index()
+    {
+        Gate::authorize('viewAny', Artist::class);
+
+        $artists = Artist::orderBy('kana')->paginate(20);
+
+        return response()->json([
+            "artists" => $artists,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        Gate::authorize('create', Artist::class);
+
+        $data = $request->validate([
+            'name' => 'required|string|max:100|unique:artists,name',
+            'kana' => 'required|string|max:100'
+        ]);
+
+        Artist::create($data);
+
+        return response()->json([
+            'message' => 'Artist created successfully'
+        ], 201);
+    }
+
+    public function update(Request $request, Artist $artist)
+    {
+        Gate::authorize('update', $artist);
+
+        $data = $request->validate([
+            'name' => 'required|string|max:100|unique:artists,name,' . $artist->id, // このidのデータは除く
+            'kana' => 'required|string|max:100'
+        ]);
+
+        $artist->update($data);
+        
+        return response()->json([
+            'message' => 'Artist updated successfully',
+        ]);
+    }
+
+    public function destroy(Artist $artist)
+    {
+        Gate::authorize('delete', $artist);
+
+        $artist->delete();
+        
+        return response()->json([
+            'message' => 'Artist deleted successfully'
+        ]);
+    }
+
 }
